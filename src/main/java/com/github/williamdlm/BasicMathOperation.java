fe5bc72263d43294cc8ab81cf5c4944f746357d9
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-03 09:22:11 +0100

    Initial commit

---
+++ src/main/java/com/github/williamdlm/BasicMathOperation.java
@@ -0,0 +1,16 @@
+package com.github.williamdlm;
+
+public class BasicMathOperation {
+
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public double divide(int x, int y) {
+        return (double) x / y;
+    }
+}
---
+++ src/main/java/com/github/williamdlm/Greets.java
@@ -0,0 +1,12 @@
+package com.github.williamdlm;
+
+public class Greets {
+
+    public String sayHi(String name) {
+        return "Hi " + name;
+    }
+
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
