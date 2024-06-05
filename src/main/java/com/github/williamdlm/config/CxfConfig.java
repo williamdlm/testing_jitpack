1a46462d6bc60697d48f6a8498af8d2eff77d1fd
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-05 16:47:33 +0100

    Feat producerCxf endpoint

--- src/main/java/com/github/williamdlm/config/CxfConfig.java
+++ src/main/java/com/github/williamdlm/config/CxfConfig.java
@@ -2,6 +2,7 @@ package com.github.williamdlm.config;
 
 import com.github.williamdlm.service.Introduce;
 import org.apache.camel.BeanInject;
+import org.apache.camel.component.cxf.common.DataFormat;
 import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 
 public class CxfConfig {
@@ -10,6 +11,14 @@ public class CxfConfig {
         CxfEndpoint cxfEndpoint = new CxfEndpoint();
         cxfEndpoint.setAddress("http://localhost:8080/introduce");
         cxfEndpoint.setServiceClass(Introduce.class);
+        return cxfEndpoint;
+    }
+
+    @BeanInject
+    public CxfEndpoint producerCxf(){
+        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setWsdlURL("cxfbean-to-log:8080/services/soapbean?wsdl");
+        cxfEndpoint.setDataFormat(DataFormat.RAW);
         return cxfEndpoint;
     }
 }
