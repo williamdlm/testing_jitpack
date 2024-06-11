84f0bc6cec97d3cb8f80018e616a1fd24b7cd5f1
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-11 11:53:48 +0100

    Added annotation EndpointInject

--- src/main/java/com/github/williamdlm/config/CxfConfig.java
+++ src/main/java/com/github/williamdlm/config/CxfConfig.java
@@ -3,11 +3,13 @@ package com.github.williamdlm.config;
 import com.github.williamdlm.service.Introduce;
 import org.apache.camel.BeanInject;
 import org.apache.camel.BindToRegistry;
+import org.apache.camel.EndpointInject;
 import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 
 public class CxfConfig {
     @BeanInject
     @BindToRegistry("introduceEndpoint")
+    @EndpointInject
     public CxfEndpoint introduceEndpoint() {
         CxfEndpoint cxfEndpoint = new CxfEndpoint();
         cxfEndpoint.setAddress("http://localhost:8080/introduce");
