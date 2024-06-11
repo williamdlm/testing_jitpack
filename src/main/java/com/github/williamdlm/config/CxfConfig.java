cd13e70a6228190531cdc1a2d3b35d8e14b33c97
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-11 10:50:06 +0100

    Added annotation BindToRegistry to CxfConfig

--- src/main/java/com/github/williamdlm/config/CxfConfig.java
+++ src/main/java/com/github/williamdlm/config/CxfConfig.java
@@ -2,10 +2,12 @@ package com.github.williamdlm.config;
 
 import com.github.williamdlm.service.Introduce;
 import org.apache.camel.BeanInject;
+import org.apache.camel.BindToRegistry;
 import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 
 public class CxfConfig {
     @BeanInject
+    @BindToRegistry("introduceEndpoint")
     public CxfEndpoint introduceEndpoint() {
         CxfEndpoint cxfEndpoint = new CxfEndpoint();
         cxfEndpoint.setAddress("http://localhost:8080/introduce");
